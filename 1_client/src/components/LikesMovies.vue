2d42d77f07b8e991fc58b3b17b5e9de27cd7681a
shinlama <jiwon.shinnn@gmail.com>
2022-11-22 17:10:11 +0900

    좋아요많은영화

--- 1_client/src/components/LikesMovies.vue
+++ 1_client/src/components/LikesMovies.vue
@@ -2,17 +2,46 @@
   <div>
     <h3>좋아요 많은 영화</h3>
     <p>많은 사람들이 좋아하는 영화를 알아보세요!</p>
-    
+    <VueSlickCarousel v-bind="settings" v-if="movies.length">
+      <div v-for="movie in movies" v-bind:key="movie.id" class="movie-list">
+        <SeasonMovieCard
+          v-bind:title="movie.title"
+          v-bind:posterPath="movie.poster_path"
+          v-bind:releaseDate="movie.release_date"
+          v-bind:movieId="movie.id"
+        />
+      </div>
+    </VueSlickCarousel>
+  
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import SeasonMovieCard from "@/components/SeasonMovieCard.vue";
+import VueSlickCarousel from "vue-slick-carousel";
+import "vue-slick-carousel/dist/vue-slick-carousel.css";
+import "vue-slick-carousel/dist/vue-slick-carousel-theme.css";
 
 export default {
+  components: {
+    SeasonMovieCard,
+    VueSlickCarousel,
+  },
   data(){
     return{
-      movies:[]
+      movies:[],
+      settings: {
+        dots: true,
+        infinite: true,
+        slidesToShow: 9,
+        slidesToScroll: 9,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        pauseOnDotsHover: true,
+        pauseOnFocus: true,
+        pauseOnHover: true,
+      },
     }
   },
   created() {
@@ -39,6 +68,9 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.movie-list {
+  margin-top: 20px;
+  margin-bottom: 30px;
+}
 </style>
--- 1_client/src/components/YourMovies.vue
+++ 1_client/src/components/YourMovies.vue
@@ -117,7 +117,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .movie-list {
   margin-top: 20px;
   margin-bottom: 30px;
